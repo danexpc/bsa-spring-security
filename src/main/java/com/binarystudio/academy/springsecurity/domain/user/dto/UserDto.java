a4d2db57a3f7eb73ce629f999b10f4cf0b743f8f
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-02 22:37:32 +0300

    Add fields to DTO about state of user

--- src/main/java/com/binarystudio/academy/springsecurity/domain/user/dto/UserDto.java
+++ src/main/java/com/binarystudio/academy/springsecurity/domain/user/dto/UserDto.java
@@ -16,6 +16,11 @@ public class UserDto {
     private final String email;
     private final String password;
     private final Set<UserRole> authorities;
+    private final boolean isAccountNonExpired;
+    private final boolean isAccountNonLocked;
+    private final boolean isCredentialsNonExpired;
+    private final boolean isEnabled;
+
 
     public static UserDto fromEntity(User user) {
         return UserDto.builder()
@@ -24,6 +29,10 @@ public class UserDto {
                 .email(user.getEmail())
                 .password(user.getPassword())
                 .authorities(user.getAuthorities())
+                .isAccountNonExpired(user.isAccountNonExpired())
+                .isAccountNonLocked(user.isAccountNonLocked())
+                .isCredentialsNonExpired(user.isCredentialsNonExpired())
+                .isEnabled(user.isEnabled())
                 .build();
     }
 }
