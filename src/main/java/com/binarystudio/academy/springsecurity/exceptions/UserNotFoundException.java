c2ec4d09b61c7fd072f9b53bc11e103b63cac0c3
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-02 22:43:06 +0300

    Create custom exceptions

---
+++ src/main/java/com/binarystudio/academy/springsecurity/exceptions/UserNotFoundException.java
@@ -0,0 +1,13 @@
+package com.binarystudio.academy.springsecurity.exceptions;
+
+public class UserNotFoundException extends RuntimeException{
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+}
