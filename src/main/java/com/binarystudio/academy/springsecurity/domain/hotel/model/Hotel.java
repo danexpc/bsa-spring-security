208780ef93e11789034046c1ad46a1d3041167bb
Dan Smahliuk <55285854+danexpc@users.noreply.github.com>
2021-07-02 22:05:51 +0300

    Add static method which creates Hotel from given parameters

--- src/main/java/com/binarystudio/academy/springsecurity/domain/hotel/model/Hotel.java
+++ src/main/java/com/binarystudio/academy/springsecurity/domain/hotel/model/Hotel.java
@@ -12,6 +12,16 @@ public class Hotel {
 	private String imageUrl;
 	private UUID ownerId;
 
+	public static Hotel of(UUID id, String name, String description, String imageUrl, UUID ownerUuid) {
+		var hotel = new Hotel();
+		hotel.setId(id);
+		hotel.setName(name);
+		hotel.setDescription(description);
+		hotel.setImageUrl(imageUrl);
+		hotel.setOwnerId(ownerUuid);
+		return hotel;
+	}
+
 	public static Hotel of(String name, String description, String imageUrl, UUID ownerUuid) {
 		var hotel = new Hotel();
 		hotel.setId(UUID.randomUUID());
